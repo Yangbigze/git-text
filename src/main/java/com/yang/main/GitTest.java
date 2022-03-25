37f054f14ad76703b1a4b150943af309f1b94eba
Yangze <Yangze@qq.com>
2022-03-25 20:45:43 +0800

    push commit

--- src/main/java/com/yang/main/GitTest.java
+++ src/main/java/com/yang/main/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("Hello git1！");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
