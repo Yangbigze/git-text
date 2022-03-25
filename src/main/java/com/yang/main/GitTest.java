75f71d472e461453512eba2c0f42fc328364ed09
Yangze <Yangze@qq.com>
2022-03-25 20:23:19 +0800

    second commit

--- src/main/java/com/yang/main/GitTest.java
+++ src/main/java/com/yang/main/GitTest.java
@@ -3,5 +3,6 @@ package com.yang.main;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git1！");
+        System.out.println("Hello git1！");
     }
 }
